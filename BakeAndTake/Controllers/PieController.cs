551c19ae0d3bc6ce4b89025c4e68e7cb93d6ebb2
Humberto Barrantes <sudohumberto@gmail.com>
2022-07-08 03:22:15 -0600

    Pie List View and ViewModel

---
+++ BakeAndTake/Controllers/PieController.cs
@@ -0,0 +1,33 @@
+﻿using BakeAndTake.Models;
+using BakeAndTake.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BakeAndTake.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public ViewResult List()
+        {
+            // Use ViewBag
+            //ViewBag.CurrentCategory = "Cheese Cakes";
+
+            // Or a ViewModel
+            PiesListViewModel piesListViewModel = new() 
+            {
+                Pies = _pieRepository.AllPies,
+                CurrentCategory = "Cheese Cakes"
+            };
+
+            return View(piesListViewModel);
+        }
+    }
+}
---
+++ BakeAndTake/ViewModels/PiesListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using BakeAndTake.Models;
+
+namespace BakeAndTake.ViewModels
+{
+    public class PiesListViewModel
+    {
+        public IEnumerable<Pie> Pies { get; set; } = null!;
+        public string CurrentCategory { get; set; } = null!;
+    }
+}
