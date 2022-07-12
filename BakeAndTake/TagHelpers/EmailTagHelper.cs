d2e0ef6c0819e69d85b0e3e318716efeefc06f52
Humberto Barrantes <sudohumberto@gmail.com>
2022-07-11 20:32:41 -0600

    Email Tag Helper

---
+++ BakeAndTake/TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace BakeAndTake.TagHelpers
+{
+    public class EmailTagHelper : TagHelper
+    {
+        public string Address { get; set; } = null!;
+        public string Content { get; set; } = null!;
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            output.Attributes.SetAttribute("href", "mailto:" + Address);
+            output.Content.SetContent(Content);
+        }
+    }
+}
