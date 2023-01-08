ede421ea0e4dba86f77de2d91bc16afb6c4c396b
Humberto Barrantes <sudohumberto@gmail.com>
2023-01-07 22:41:26 -0600

    Adding Navigation to the Site

---
+++ BakeAndTake/BakeAndTake/ViewModels/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using BakeAndTake.Models;
+
+namespace BakeAndTake.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Pie> PiesOfTheWeek { get; } = null!;
+
+        public HomeViewModel(IEnumerable<Pie> piesOfTheWeek)
+        {
+            PiesOfTheWeek = piesOfTheWeek;
+        }
+    }
+}
