a3117911eb433595ae198273b357190c67cc4434
Humberto Barrantes <sudohumberto@gmail.com>
2023-01-08 17:46:05 -0600

    Creating ViewComponents

--- BakeAndTake/BakeAndTake/Controllers/PieController.cs
+++ BakeAndTake/BakeAndTake/Controllers/PieController.cs
@@ -1,4 +1,5 @@
-﻿using BakeAndTake.Repositories.Abstract;
+﻿using BakeAndTake.Models;
+using BakeAndTake.Repositories.Abstract;
 using BakeAndTake.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
@@ -15,10 +16,32 @@ namespace BakeAndTake.Controllers
             _categoryRepository = categoryRepository;
         }
 
-        public IActionResult List()
+        //public IActionResult List()
+        //{
+        //    PieListViewModel piesListViewModel = new(_pieRepository.AllPies, "Cheese cakes");
+        //    return View(piesListViewModel);
+        //}
+
+        public IActionResult List(string category)
         {
-            PieListViewModel piesListViewModel = new(_pieRepository.AllPies, "Cheese cakes");
-            return View(piesListViewModel);
+            IEnumerable<Pie> pies;
+            string? currentCategory;
+
+            if (string.IsNullOrEmpty(category))
+            {
+                pies = _pieRepository.AllPies.OrderBy(p => p.PieId);
+                currentCategory = "All pies";
+            }
+            else
+            {
+                pies = _pieRepository.AllPies
+                    .Where(p => p.Category.CategoryName == category)
+                    .OrderBy(p => p.PieId);
+                currentCategory = _categoryRepository.AllCategories
+                    .FirstOrDefault(c => c.CategoryName == category)?.CategoryName;
+            }
+
+            return View(new PieListViewModel(pies, currentCategory));
         }
 
         public IActionResult Details(int id)
