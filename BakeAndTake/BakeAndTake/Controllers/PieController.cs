db0e4fefec01f13333661cb76be342c5d2325471
Humberto Barrantes <sudohumberto@gmail.com>
2023-01-07 18:14:52 -0600

    Adding the controller

---
+++ BakeAndTake/BakeAndTake/Controllers/PieController.cs
@@ -0,0 +1,22 @@
+﻿using BakeAndTake.Repositories.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BakeAndTake.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public IActionResult List()
+        {
+            return View(_pieRepository.AllPies);
+        }
+    }
+}
