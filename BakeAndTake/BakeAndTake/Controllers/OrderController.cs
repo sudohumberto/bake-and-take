37f1aae573fa6ce090068be7dfd0d1a4e48e09fc
Humberto Barrantes <sudohumberto@gmail.com>
2023-01-08 19:52:57 -0600

    Accessing Posted Data

--- BakeAndTake/BakeAndTake/Controllers/OrderController.cs
+++ BakeAndTake/BakeAndTake/Controllers/OrderController.cs
@@ -19,5 +19,31 @@ namespace BakeAndTake.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Checkout(Order order)
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems= items;
+
+            if (_shoppingCart.ShoppingCartItems.Count == 0)
+            {
+                ModelState.AddModelError("", "Your cart is empty, add some pies first");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _orderRepository.CreateOrder(order);
+                _shoppingCart.ClearCart();
+                return RedirectToAction("CheckoutComplete");
+            }
+            return View(order);
+        }
+
+        public IActionResult CheckoutComplete()
+        {
+            ViewBag.CheckoutCompleteMessage = "Thanks for your order. You'll soon enjoy our delicious pies!";
+            return View();
+        }
     }
 }
